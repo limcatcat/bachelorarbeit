0a49f5375f79537f6aa8dfe5372ad216c02d4cbb
soyeon <limcatcat@gmail.com>
2026-03-12 00:59:24 +0100

    set up langfuse prompt fetching mechanism

---
+++ src/fetch_prompts.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from langfuse import get_client
+
+load_dotenv()
+
+langfuse = get_client(
+    public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
+    secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
+    host=os.getenv("LANGFUSE_BASE_URL"),
+)
+
+def fetch_prompts(system_prompt_name: str, user_prompt_name: str, context: str, question: str) -> tuple[str, str]:
+    system_prompt = langfuse.get_prompt(system_prompt_name)
+    user_prompt = langfuse.get_prompt(user_prompt_name)
+
+    compiled_system_prompt = system_prompt.compile()
+    compiled_user_prompt = user_prompt.compile(
+        context=context,
+        question=question,
+    )
+
+    return compiled_system_prompt, compiled_user_prompt
